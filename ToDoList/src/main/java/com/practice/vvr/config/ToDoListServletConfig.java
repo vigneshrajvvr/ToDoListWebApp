08ab2153f83fe371588c6a3dbc1ba63a30c3ad0a
Vicky <vvigneshrajvvr@gmail.com>
2020-07-03 20:14:27 +0530

    Basic MVC flow setup

---
+++ ToDoList/src/main/java/com/practice/vvr/config/ToDoListServletConfig.java
@@ -0,0 +1,23 @@
+package com.practice.vvr.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.practice.vvr")
+public class ToDoListServletConfig {
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	
+}
---
+++ ToDoList/src/main/java/com/practice/vvr/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.practice.vvr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String getHomePage() {
+		return "home";
+	}
+	
+}
