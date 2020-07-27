2f1ec33efe158398d5f34f9519752a10ca419b3b
Vignesh Raj V <38003146+vigneshrajvvr@users.noreply.github.com>
2020-07-27 23:53:40 +0530

    Update Schema.sql

--- SQL scripts/Schema.sql
+++ SQL scripts/Schema.sql
@@ -1,12 +1,12 @@
 -- User table
 CREATE TABLE users(
-	id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     mail_id VARCHAR(50)
 );
 
 -- Task table
 CREATE TABLE tasks(
-	id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     task_header VARCHAR(100),
     task_description VARCHAR(500),
     date_added DATE DEFAULT NOW(),
