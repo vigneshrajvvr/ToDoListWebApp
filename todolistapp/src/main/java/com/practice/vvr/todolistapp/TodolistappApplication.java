e5d14506d94036d2466a0e002a9b2fc6823695c4
vigneshrajvvr <vvigneshrajvvr@gmail.com>
2020-09-28 20:46:04 +0530

    Spring boot todolist webapp

---
+++ todolistapp/src/main/java/com/practice/vvr/todolistapp/TodolistappApplication.java
@@ -0,0 +1,13 @@
+package com.practice.vvr.todolistapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistappApplication.class, args);
+	}
+
+}
---
+++ todolistapp/src/test/java/com/practice/vvr/todolistapp/TodolistappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.vvr.todolistapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
